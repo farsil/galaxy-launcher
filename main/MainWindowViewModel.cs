e7f1d6497f4b6d4dc70447f3f2e0accd18385ba3
Marco Buzzanca <marco.bzn@gmail.com>
2026-03-31 22:29:37 +0200

    Cleanup process on launcher exit

--- main/MainWindowViewModel.cs
+++ main/MainWindowViewModel.cs
@@ -16,6 +16,7 @@ public sealed partial class MainWindowViewModel()
 {
     private readonly ObservableCollection<Program> _programs = [];
     private string _dosboxPath = string.Empty;
+    private Process? _dosboxProcess;
 
     public IEnumerable<Program> FilteredPrograms =>
         string.IsNullOrWhiteSpace(SearchText)
@@ -47,18 +48,22 @@ public sealed partial class MainWindowViewModel()
     [RelayCommand]
     private void StartProgram(Program program)
     {
-        Process.Start(_dosboxPath, ["--conf", program.ConfigPath, "--working-dir", program.Path]);
+        _dosboxProcess = Process.Start(_dosboxPath, ["--conf", program.ConfigPath, "--working-dir", program.Path]);
     }
 
     protected override void OnActivated()
     {
         base.OnActivated();
+        
         Messenger.Send(new MainWindowActiveChangeMessage(true));
     }
 
     protected override void OnDeactivated()
     {
         base.OnDeactivated();
+        
+        if (_dosboxProcess?.HasExited == false)
+            _dosboxProcess.Kill();
         Messenger.Send(new MainWindowActiveChangeMessage(false));
     }
 }
